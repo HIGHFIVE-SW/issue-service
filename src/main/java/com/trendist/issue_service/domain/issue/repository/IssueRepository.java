80637eeaff8e8e63bcbfbf70a55c42150b906fb2
1winhyun <jimking1@naver.com>
2025-05-12 16:01:02 +0900

    feat: 이슈 전체 조회 repository 작성

---
+++ src/main/java/com/trendist/issue_service/domain/issue/repository/IssueRepository.java
@@ -0,0 +1,10 @@
+package com.trendist.issue_service.domain.issue.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trendist.issue_service.domain.issue.domain.Issue;
+
+public interface IssueRepository extends JpaRepository<Issue, UUID> {
+}
