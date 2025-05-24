d6d14dbd66578b651a589f8c6f12bb91d12ab4d2
1winhyun <jimking1@naver.com>
2025-05-24 16:23:55 +0900

    chore: dev 서버 환경 및 민감한 정보 jasypt 암호화 진행

--- src/test/java/com/trendist/issue_service/IssueServiceApplicationTests.java
+++ src/test/java/com/trendist/issue_service/IssueServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.trendist.issue_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = IssueServiceApplicationTests.class)
 class IssueServiceApplicationTests {
 
 	@Test
